e13e1689a6ba3d73c4b2be53316b7f11e2413bb5
Krek0204 <vlad020406@yandex.ru>
2025-06-08 17:18:48 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,7 +3,7 @@
 #include <fstream>
 #include <iostream>
 #include <locale>
-
+#include <vector>
 #include "tree.h"
 
 void gettingAllPerms(Node* root, std::vector<std::vector<char>>& res,
@@ -24,8 +24,55 @@ std::vector<std::vector<char>> getAllPerms(PMTree& tree) {
   return result;
 }
 
+bool Perm1DLC(Node* root, std::vector<char>& res, int& cur) {
+  if (root->data != 0) res.push_back(root->data);
+  if (root->heirs.empty()) {
+    cur--;
+    if (cur == 0) return true;
+  } else {
+    for (auto& it : root->heirs) {
+      if (Perm1DLC(it, res, cur)) return true;
+    }
+  }
+  res.pop_back();
+  return false;
+}
 
-std::vector<char> getPerm1(PMTree& tree, int pos) {
-  std::vector<std::vector<char>> perms = getAllPerms(tree);
-  return perms[pos - 1];
+
+std::vector<char> getPerm1(PMTree& tree, int pos) { std::vector<char> result;
+  int cur = pos;
+  std::vector<char> result;
+  Node* rt = tree.getRoot();
+  for (auto* it : rt->heirs) {
+    if (Perm1DLC(rt, result, cur)) return result;
+  }
+  return {};
+}
+
+bool Perm2Finder(Node* root, std::vector<char>& res, int cnt) {
+  if (root->heirs.empty()) return true;
+  int currentP = fact(root->heirs.size() - 1);
+  for (auto& it : root->heirs) {
+    if (cnt < currentP) {
+      res.push_back(it->data);
+      return Perm2Finder(it, res, cnt);
+    }
+    cnt -= currentP;
+  }
+  return false;
+}
+
+std::vector<char> getPerm2(PMTree& tree, int pos) { int cnt = pos - 1;
+  std::vector<char> result;
+  Node* rt = tree.getRoot();
+  for (auto& it : rt->heirs) {
+    int currentP = fact(rt->heirs.size() - 1);
+    if (cnt < currentP) {
+      result.push_back(it->data);
+      if (Perm2Finder(it, result, cnt)) return result;
+      break;
+    }
+    cnt -= currentP;
+  }
+  return result;
 }
